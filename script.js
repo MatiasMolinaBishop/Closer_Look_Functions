19af93923448b8ba9070927c6b0e23846c7cf976
Matias Molina Bishop <molibi01@gmail.com>
2023-09-25 08:18:49 +0200

    Functions Accepting Callback Functions 1.0

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const checkIn = (flightNum, passanger) => {
   passanger.name = 'Mr.' + passanger.name;
 
   if (passanger.passport === 24739479284) {
-    alert('Checked in!');
+    //alert('Checked in!');
     console.log('Checked in!');
   } else {
     //alert('Wrong passport!');
@@ -71,3 +71,35 @@ const flightNum = flight;
 const passanger = jonas;
 //With the object we are copying the reference to that object in the memory key but they both point to the same object in memory
 //KEEP IN MIND THIS BEHAVIOR WHEN WORKING WITH OBJECTS AND FUNCTION!
+
+///////////////////////////////////////
+// Functions Accepting Callback Functions
+
+//This funtion transform a string into a a single word replacing all the spaces with ''
+const oneWord = str => {
+  const word = str.replace(/ /g, '').toLowerCase();
+  console.log(word);
+  return word;
+};
+
+//oneWord('hola vKLJN ');
+//This function transform the first word of a str / word into upper case
+const upperFirstWord = str => {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+//Higher order funtion as it takes another function as an argument
+
+const transformer = function (str, fn) {
+  console.log(`Originl string: ${str}`);
+  console.log(`Transform string: ${fn(str)}`);
+
+  //Function have methids and properties
+  console.log(`Transformer function ${fn.name}`);
+};
+
+//Note how we only pass in the function as an arument. The function is not called
+//It will be the transformer function calling the function
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
